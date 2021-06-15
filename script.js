712fcba942219b3b79eeb24bbe15043d08b6f5e7
MakrMohamed <84314376+MakrMohamed@users.noreply.github.com>
2021-06-15 10:40:00 +0200

    Add files via upload
    6e Javascript Opdr - Functions

---
+++ script.js
@@ -0,0 +1,20 @@
+const makePizza = function () {
+    console.log("Make dough");
+    console.log("Put tomato sauce on dough");
+    console.log("Add cheese");
+    console.log("Add toppings");
+    console.log("Put it in the oven for 20 min on 200 Celsius");
+};
+
+const makeSushi = function () {
+    console.log("Boil rice");
+    console.log("Cut fresh fish");
+    console.log("Roll fish pieces in rice");
+    console.log("Add wassabi");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
